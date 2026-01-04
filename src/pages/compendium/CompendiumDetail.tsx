07f2e5305f3bba07bda8ffdced598aa8a2e40358
Tomoshibi1125 <jjcaldwelljr@gmail.com>
2026-01-04 05:34:58 -0800

    fix: Remove unused eslint-disable directive

--- src/pages/compendium/CompendiumDetail.tsx
+++ src/pages/compendium/CompendiumDetail.tsx
@@ -89,7 +89,6 @@ const CompendiumDetail = () => {
     // Cast entry to any to avoid strict type checking for dynamic data
     const data = entry as Record<string, unknown>;
 
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     switch (type) {
       case 'jobs':
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
