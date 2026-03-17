c01009007e243742c2475fecdffcf39344d738af
Tomoshibi1125 <jjcaldwelljr@gmail.com>
2026-03-17 15:16:14 -0700

    chore: remove unused indexes detected by supabase diff

---
+++ supabase/migrations/20260317221540_final_verification.sql
@@ -0,0 +1,41 @@
+drop index if exists "public"."idx_campaign_member_characters_campaign_id";
+
+drop index if exists "public"."idx_campaign_member_characters_campaign_member_id";
+
+drop index if exists "public"."idx_campaign_rules_campaign_id";
+
+drop index if exists "public"."idx_campaign_sourcebook_shares_campaign_id";
+
+drop index if exists "public"."idx_character_equipment_container_id";
+
+drop index if exists "public"."idx_character_extras_monster_id";
+
+drop index if exists "public"."idx_character_feature_choices_character_id";
+
+drop index if exists "public"."idx_character_monarch_unlocks_character_id";
+
+drop index if exists "public"."idx_character_rune_knowledge_character_id";
+
+drop index if exists "public"."idx_character_shadow_soldiers_character_id";
+
+drop index if exists "public"."idx_character_techniques_character_id";
+
+drop index if exists "public"."idx_compendium_feature_choice_groups_feature_id";
+
+drop index if exists "public"."idx_compendium_feature_choice_options_group_id";
+
+drop index if exists "public"."idx_homebrew_content_versions_homebrew_id";
+
+drop index if exists "public"."idx_marketplace_downloads_item_id";
+
+drop index if exists "public"."idx_marketplace_reviews_item_id";
+
+drop index if exists "public"."idx_user_marketplace_entitlements_user_id";
+
+drop index if exists "public"."idx_user_sourcebook_entitlements_user_id";
+
+drop index if exists "public"."idx_vtt_fog_state_campaign_id";
+
+drop index if exists "public"."idx_vtt_tokens_owned_by_user_id";
+
+
